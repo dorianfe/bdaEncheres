6e32f9273acdbe4d52946e7e9354ae1d4d0e4cdf
dorian <dorian.fenestre2022@camus-eni.fr>
2022-08-30 09:12:07 +0200

    listerUtilisateur

--- src/main/java/fr/bda/bdaencheres/bll/GestionUtilisateur.java
+++ src/main/java/fr/bda/bdaencheres/bll/GestionUtilisateur.java
@@ -1,8 +1,12 @@
 package fr.bda.bdaencheres.bll;
 
+import fr.bda.bdaencheres.bo.ArticleVendu;
+import fr.bda.bdaencheres.bo.Utilisateur;
 import fr.bda.bdaencheres.dal.UtilisateurDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GestionUtilisateur {
@@ -10,6 +14,9 @@ public class GestionUtilisateur {
     @Autowired
     UtilisateurDAO dao;
 
+    public List<Utilisateur> listerUtilisateurs() {
 
+        return dao.findAll();
+    }
 
 }
--- src/main/java/fr/bda/bdaencheres/dal/UtilisateurDAO.java
+++ src/main/java/fr/bda/bdaencheres/dal/UtilisateurDAO.java
@@ -3,6 +3,9 @@ package fr.bda.bdaencheres.dal;
 import fr.bda.bdaencheres.bo.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UtilisateurDAO extends JpaRepository<Utilisateur, Integer> {
+    List<Utilisateur> findByOrderByIdDesc();
 
 }
