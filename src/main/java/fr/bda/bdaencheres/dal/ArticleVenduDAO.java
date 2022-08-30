61fc2db26df8e78c7b3105a30d20e2878d731762
Ackoy Bella <ackoybella.leckey2022@campus-eni.fr>
2022-08-30 15:17:06 +0200

    ajout list article et utilisateur dans le DAO

--- src/main/java/fr/bda/bdaencheres/dal/ArticleVenduDAO.java
+++ src/main/java/fr/bda/bdaencheres/dal/ArticleVenduDAO.java
@@ -1,8 +1,29 @@
 package fr.bda.bdaencheres.dal;
 
 import fr.bda.bdaencheres.bo.ArticleVendu;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleVenduDAO extends JpaRepository<ArticleVendu, Integer> {
+	
+	List<ArticleVendu> findByOrderByPrixVenteAsc();
+	List<ArticleVendu> findByOrderByPrixVenteDesc();
+	List<ArticleVendu> findByOrderByEtatVenteAsc();
+	List<ArticleVendu> findByOrderByEtatVenteDesc();
+	List<ArticleVendu> findByOrderByDateDebutEncheresAsc();
+	List<ArticleVendu> findByOrderByDateDebutEncheresDesc();
+	List<ArticleVendu> findByOrderByDateFinEncheresAsc();
+	List<ArticleVendu> findByOrderByDateFinEncheresDesc();
+	List<ArticleVendu> findByOrderByDescriptionAsc();
+	List<ArticleVendu> findByOrderByDescriptionDesc();
+	
+	
+	
+	
+	
+	
+	
 
 }
